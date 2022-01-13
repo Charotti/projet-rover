4bb1aba0ea22ad48916ff75ea44d291d5243b5ea
Charles <charles.dubois.h@gmail.com>
2022-01-13 14:38:07 +0100

    backward verification of string

--- projet.js
+++ projet.js
@@ -1,6 +1,6 @@
 //                                       Installation prompt
 const prompt = require("prompt");
-prompt.start()
+prompt.start();
 
 //                                           Etape 1 déclaration Grid
 
@@ -104,9 +104,46 @@ function moveForward(myRover) {
             break;
     }
     grid[rover.y][rover.x] = rover.direction; // fait réapparaitre le rover à la position modifiée précedement
-
 }
-//moveForward(rover);
+// Bonus 2                      Move backward
+
+function moveBackward(myRover) {
+    switch (myRover.direction) {
+        case "N":
+            if (myRover.y === 9) { // guard pour ne pas sortir du talbeau
+                console.log("Vous sortez de la grille")
+            } else {
+                grid[rover.y][rover.x] = " "; //           Fait disparaitre le rover 
+                myRover.y += 1; //                 Fait déplacer le rover
+            }
+            break;
+        case "E":
+            if (myRover.x === 0) {
+                console.log("Vous sortez de la grille")
+            } else {
+                grid[rover.y][rover.x] = " ";
+                myRover.x -= 1;
+            }
+            break;
+        case "S":
+            if (myRover.y === 0) {
+                console.log("Vous sortez de la grille")
+            } else {
+                grid[rover.y][rover.x] = " ";
+                myRover.y -= 1;
+            }
+            break;
+        case "W":
+            if (myRover.x === 9) {
+                console.log("Vous sortez de la grille")
+            } else {
+                grid[rover.y][rover.x] = " ";
+                myRover.x += 1;
+            }
+            break;
+    }
+    grid[rover.y][rover.x] = rover.direction; // fait réapparaitre le rover à la position modifiée précedement
+}
 
 
 //                                    Position du rover dans mon tableau
@@ -122,37 +159,43 @@ function pilotRover(str) {
     for (let i = 0; i < str.length; i++) {
 
         if (str.charAt(i) === "l") {
-            turnLeft(rover)
-                // console.log("c'est un l")
+            turnLeft(rover);
+            // console.log("c'est un l")
         } else if (str.charAt(i) === "r") {
             // console.log("c'est un r")
-            turnRight(rover)
+            turnRight(rover);
         } else if (str.charAt(i) === "f") {
             rover.travelLog.push(`Position ${i} : ${rover.y}y / ${rover.x}x `) // historique des positions si le rover se déplace
             moveForward(rover);
-            console.log(rover.travelLog)
+            console.log(rover.travelLog);
+        } else if (str.charAt(i) === "b") {
+            rover.travelLog.push(`Position ${i} : ${rover.y}y / ${rover.x}x `) // historique des positions si le rover se déplace
+            moveBackward(rover);
+            console.log(rover.travelLog);
         }
     }
     console.table(grid);
+
 }
-
-
 
 const conditions = [{
     name: "letter",
-    description: "Play with a f to forward, a l to turn left, a r to turn right",
+    description: "Play with a f to forward, a l to turn left, a r to turn right and a b to backward",
     type: "string",
-    pattern: /[l|r|f]/i,
-    message: "Only the r, th l and the f are valid",
+    pattern: /[l|r|f|b]/i,
+    message: "Only  r,  l , f and b are valid",
     require: true
-}]
+}];
 
 function launch() {
-    prompt.get(conditions,
+    prompt.get(conditions, // Paramètres auquel doit correspondre la lettre entrée dans prompt
         function(err, result) {
-            pilotRover(result.letter)
-            launch()
+            if (err) {
+                return console.log("Error " + err) //     Guard
+            }
+            pilotRover(result.letter) //    Donne à ma fonction piloteRover la lettre entré dans prompt
+            launch() //                   relance le jeu
         })
 }
 
-launch()
+launch();
